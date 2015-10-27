5dd73aa5d0d4ea040b0274b3938f3912970dcc12
Ryan Wilson <rww9008@rit.edu>
2015-10-27 16:13:23 -0400

    Got csv parsing working correctly. Python library would not work. Now just need to implement functions to read and write objects to database.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 
 import sys
-
+from db import DBManager
+from csv import DictReader
 
 """
 Assumes that the input file is of proper format.
@@ -13,8 +14,66 @@ def generateReport(dbManager):
     pass
 
 
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+
+def parseCSVLine(line, headerNames):
+    entry = {}
+    i = 0
+    currentItem = ""
+    openQuote = False
+    for char in line:
+        if char == ',' and not openQuote:
+            entry[headerNames[i]] = currentItem
+            i += 1
+            currentItem = ""
+        elif char == '"' and not openQuote:
+            openQuote = True
+        elif char == '"' and openQuote:
+            openQuote = False
+        else:
+            currentItem += char
+
+    return entry
+
+
 def main():
-    db = DBManager()
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        csvFile = open(path, encoding='utf-8').read()
+
+        contents = []
+        currentLine = ""
+        for char in csvFile:
+            if is_ascii(char):
+                if char == "\n":
+                    currentLine += char
+                    contents.append(currentLine)
+                    currentLine = ""
+                else:
+                    currentLine += char
+            else:
+                pass
+
+        firstLine = contents[0]
+        headerLine = contents[1]
+        fieldNames = headerLine.replace('"', '').strip().split(',')
+
+        contents = contents[2::]
+
+        entries = []
+        for line in contents:
+            entry = parseCSVLine(line, fieldNames)
+            entries.append(entry)
+
+        # FINALLY!!! Have parsed CSV. Now relate to search and put into database.
+
+        db = DBManager()
+        
+
+    else:
+        print("Incorrect arguments.")
 
 
 if __name__ == "__main__":
