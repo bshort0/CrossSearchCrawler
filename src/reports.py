670b18d0e92723f16c0892d8f15977613dcffe2b
Ryan Wilson <rww9008@rit.edu>
2015-12-06 20:48:41 -0500

    Documented parse.py

--- src/reports.py
+++ src/reports.py
@@ -1,39 +1,61 @@
 
 from db import DBManager
 
+
+"""
+Generates a report that shows the overlap between any
+two searches.
+"""
 def generateReportCrossover(db):
 
     listTable = []
     searches = db.getSearches()
 
+    # Setting up the first CSV line in the report
     firstLine = ['Total', 'Total']
     for s in searches:
         firstLine.append(s[1])
 
     listTable.append(firstLine)
 
+    # Preparing the second line. Line of totals so are just getting single counts
     secondLine = ['Total', '']
     for s in searches:
+        # s[0] is the text of a search query
         secondLine.append(str(len(db.getSearchResults(s[0]))))
 
     listTable.append(secondLine)
 
     for s in searches:
-        line = [s[1], str(len(db.getSearchResults(s[0])))]
+        # s[0] is the id of a search in the database
+        # s[1] is the text of the search query 
+        line = [s[0], str(len(db.getSearchResults(s[1])))]
         for other in searches:
+            # The function being called takes two search IDs and gets 
+            # the number of overlapping publications
             line.append(str(len(db.getOverlappingResults(s[0], other[0]))))
         listTable.append(line)
 
     return listTable
 
+
+"""
+Generates a report showing the number of publications 
+for a search for each year.
+
+This function is essentially a printing function for the return
+value of DBManager.getSearchesByYear()
+"""
 def generateReportByYear(db):
     
     listTable = []
 
     years = {}
+    # The real meat of this function
     searches = db.getSearchesByYear()
     keys = searches.keys()
 
+    # Create the first line of search text
     firstLine = ['Year']
     for s in keys:
         firstLine.append(s)
@@ -46,6 +68,7 @@ def generateReportByYear(db):
 
     yearKeys = sorted(years.keys())
 
+    # for each year, create the line and write the counts for each search
     for y in yearKeys:
         line = []
         line.append(y)
@@ -56,6 +79,9 @@ def generateReportByYear(db):
     return listTable
 
 
+"""
+Generates a report for the number of contributing authors compared to each search.
+"""
 def generateAuthorReport(db):
 
     listTable = []
