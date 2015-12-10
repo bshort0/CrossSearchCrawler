0ff507cd8bdfd70afeddae69df98b18fdc9a38e8
Ryan Wilson <rww9008@rit.edu>
2015-12-09 19:49:08 -0500

    created skeleton for basic functions in the shell.

--- src/shell.py
+++ src/shell.py
@@ -1,7 +1,40 @@
 
 
-def run(db):
+def parseOverlapCommand(db, command):
 	pass
+
+
+
+def parseCountCommand(db, command):
+	pass
+
+
+
+def help():
+	print("Help")
+
+
+
+def run(db):
+
+	print("Entering shell. See documentation or type 'help' for available commands.\nType \"quit\" or \"exit\" to leave.)
+	command = input(">")
+	while command.lower() != "quit" and command.lower() != "exit":
+
+		if command.startswith("overlap"):
+			parseOverlapCommand(db, command)
+
+		elif command.startswith("count"):
+			parseCountCommand(db, command)
+
+		elif command.startswith("help"):
+			help()
+
+		else:
+			print('"' + command + '" is not supported. See documentation or type help for supported commands.')
+		command = input(">")
+
+
 
 def main():
 	pass
